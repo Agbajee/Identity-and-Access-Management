abb4ca2aa0fbf542c3d33c4a6ebd8e7768dc68f5
Ayomide Agbaje <agbajee835@gmail.com>
2022-07-10 02:25:00 +0100

    Basic Flakk Auth

--- lesson-1-Foundation/FlaskRecap/FlaskRecap.py
+++ lesson-1-Foundation/FlaskRecap/FlaskRecap.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
+from encodings.utf_8 import encode
 from flask import Flask, request, jsonify, abort
+import jwt
+import base64
+import json
+from jose import jwt
+from urllib.request import urlopen
+
+AUTH0_DOMAIN = 'devagbaje.us.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'image'
+AUTH_LINK = 'https://devagbaje.us.auth0.com/authorize?audience=image&response_type=token&client_id=zHXITxdbfKqY1vYaaZSzAPqGxLMJVn4j&redirect_uri=https://127.0.0.1:8080/login-result'
 
 app = Flask(__name__)
 
@@ -13,6 +24,26 @@ greetings = {
             'he': 'שלום',
             'ja': 'こんにちは'
             }
+
+@app.route('/', methods=['GET'])
+def hello():
+    payload = {'school':'udacity'}
+    algo = 'HS256'
+    secret = 'learning'
+
+    encoded_jwt = jwt.encode(payload, secret, algorithm=algo)
+
+    return encoded_jwt
+
+@app.route('/header', methods=['GET'])
+def headers():
+    auth_header = request.headers['Authorization']
+
+    # get the token
+    header_parts = auth_header.split(' ')[1]
+    print(header_parts)
+
+    return header_parts
 
 @app.route('/greeting', methods=['GET'])
 def greeting_all():
@@ -33,3 +64,4 @@ def greeting_add():
         abort(422)
     greetings[info['lang']] = info['greeting']
     return jsonify({'greetings':greetings})
+
